694668fa2510f1ee862e637c053d432a363e364c
victorpepp <victorpimenta@poli.ufrj.br>
2016-11-22 21:12:45 -0200

    Update FFTmonitor.py
    1- In the future, as we will work with many characteristic frequencies, I've changed the signal generator function to demand for a list of values, instead of three integers.

--- FFTmonitor.py
+++ FFTmonitor.py
@@ -4,38 +4,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.fftpack import fft
+import scipy.signal as sgn
+
 
 """
 functions space
 """
 #create a sin function signal with 3 base frequencies and its 3 first harmonics
-def signalgenerator(freq1, freq2 = 0, freq3 = 0):
+def signalgenerator(frequencies, Fs, noise = True, signal = 0):
     
-    #base frequencies
-    wave1 = (amplitude) * np.sin(2.0*np.pi*freq1*t)
-    wave2 = (amplitude) * np.sin(2.0*np.pi*freq2*t)
-    wave3 = (amplitude) * np.sin(2.0*np.pi*freq3*t)
-    
-    #1st harmonics
-    wave4 = (amplitude/2) * np.sin(2.0*np.pi*2*freq1*t)
-    wave5 = (amplitude/2) * np.sin(2.0*np.pi*2*freq2*t)
-    wave6 = (amplitude/2) * np.sin(2.0*np.pi*2*freq3*t)
+    for freq in frequencies:
+        #base frequencies
+        wave = (amplitude) * np.sin(2.0*np.pi*freq*t)
+        
+        #1st harmonics
+        harmonic1 = (amplitude/2) * np.sin(2.0*np.pi*2*freq*t)
+        
+        signal += wave + harmonic1
 
-    
     #creates noise
     # 0 is the mean of the normal distribution you are choosing from
     # 1 is the standard deviation of the normal distribution
     # last one is the number of elements you get in array noise
     
-    noise = np.random.normal(0,1,40000)
-#TODO: create a method that generates more waves and maybe add noise
+    if noise is True:
+        noise = np.random.normal(0,1,Fs)
+        signal += noise
 
-    
-    signal = wave1 + wave2 + wave3 + wave4 + wave5 + wave6 + noise
     return signal
-
-
-
 
 # Given 2 frequency values, creates a plot of the FFT in the interval
 # delimited by them
@@ -64,23 +60,23 @@ values space
 amplitude = 1.0
 
 #in Hz
-freq1 = 1000.0
-freq2 = 755.0
-freq3 = 355.0
+
+frequencies = [1000.0, 755.0, 355.0]
 
 t = np.arange(0.0,1,25e-6)
+
 #TODO: whats the best step for t variable so that we can have a good measuement?(considering the amount of harmonics)
+
 """
 process
 """
 
-signal = signalgenerator(freq1,freq2,freq3)
-    
 N = np.int(np.prod(t.shape))# list length
 Fs = 1/(t[1]-t[0]) 	# sample frequency
 T = 1/Fs;
-print "# Samples:", N
+print("# Samples:", N)
 
+signal = signalgenerator(frequencies, Fs)
 
 """
 plots
@@ -99,7 +95,7 @@ ax1.set_title("Time Domain")
 #FFT
 fig2 = plt.figure(2)
 ax2 = fig2.add_subplot(111)
-xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
+xf = np.linspace(0.0, 1.0/(2*T), N/2)
 yf = fft(signal)
 ax2.plot(xf, 2.0/N * np.abs(yf[0:np.int(N/2)]))
 ax2.grid()
